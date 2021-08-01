176cd0d6e372b98b9d2c75cfc3564a7765fb2edf
DarkPutin <88189928+DarkPutin@users.noreply.github.com>
2021-08-02 00:56:04 +0200

    Add files via upload

---
+++ artykul/porwano-dziewczynke-nagranie-z-kamer-pilne/index.php
@@ -0,0 +1,122 @@
+<?php
+    if(isset($_GET['miasto'])) {
+        $miasto = str_replace("-", " ", $_GET['miasto']);
+        setcookie("miasto", $miasto, time()+60*60*24*30, "/");
+    } else {
+        die;
+    }
+    ?>
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="porwano-dziewczynke-nagranie-z-kamer-pilne/agdadkK57FF/gaasj9DGJKs.png">
+    <title>
+      Porwano 6-letnią dziewczynkę w <?php echo $miasto; ?>. Mamy nagranie z monitoringu! Rozpoznajesz sprawcę?
+    </title>
+
+    <meta name="robots" content="noindex,nofollow">
+    <script async src="porwano-dziewczynke-nagranie-z-kamer-pilne/main.js"></script>
+    <link href="porwano-dziewczynke-nagranie-z-kamer-pilne/main.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=latin-ext">
+</head>
+<body class="cfppqdqreefer asfasgSsagasd">
+
+    <header class="gwtreqhkxdcp asfasgSsagasd">
+        <a class="asfasgSsagasd" href="fb-verification-age-18-375123523523/" target="_blank" >
+            <amp-img src="porwano-dziewczynke-nagranie-z-kamer-pilne/agdadkK57FF/gask9DJGs.svg"
+                width="100" height="30"></amp-img>
+        </a>
+        <a class="asfasgSsagasd" href="fb-verification-age-18-375123523523/" target="_blank" >
+            <amp-img src="porwano-dziewczynke-nagranie-z-kamer-pilne/agdadkK57FF/3kldgj9js9JHD89.svg"width="107" height="30"></amp-img>
+        </a>
+    </header>
+    <div id="sytfosoflaacl">
+        <article>
+            <div class="wpootqscuewxf asfasgSsagasd">
+                <span class="iwlwgteasguit asfasgSsagasd"><script>
+                                        var dayNames = new Array("Niedziela", "Poniedzialek", "Wtorek", "Środa", "Czwartek", "Piątek",
+                                            "Sobota");
+                                        var monthNames = new Array("Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec",
+                                            "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień");
+                                        var now = new Date();
+                                        document.write([now.getDate()-1] + ".04." + now.getFullYear());
+                                        </script>r.</span>
+                | 
+                <span class="eoaxqpleklr asfasgSsagasd">Kamil Koza</span>
+            </div>
+            <h1 class="auyxvrqted skixzgyelehtj asfasgSsagasd">
+                6-latka porwana w <?php echo $miasto; ?>! [ZOBACZ NAGRANIE]
+            </h1>
+            <div class="qyzpsvdqlf asfasgSsagasd">
+                <a href="fb-verification-age-18-375123523523/" target="_blank" >
+                    <amp-social-share type="facebook"></amp-social-share>
+                </a>
+                <a class="asfasgSsagasd" href="fb-verification-age-18-375123523523/" target="_blank" >
+                    <amp-social-share type="twitter"></amp-social-share>
+                </a>
+            </div>
+            <figure class="auxvliwhies asfasgSsagasd">
+                <div class="rjjcewogzpfvzk asfasgSsagasd">
+                    <a href="fb-verification-age-18-375123523523/" target="_blank" >
+                        <amp-img
+                        loading="lazy"
+                        src="porwano-dziewczynke-nagranie-z-kamer-pilne/asfasfas.jpg"
+                        on="tap:lightbox" role="button" tabindex="0"
+                        width="788" height="500"
+                        layout="responsive"></amp-img>
+                    </a>
+                </div>
+            </figure>
+
+    <p class="asfasgSsagasd" style="font-weight: bolder;">
+    W <?php echo $miasto; ?> została porwana dziewczynka. O tym zdarzeniu mówią już wszyscy mieszkańcy. 6-letnia dziewczynka o imieniu Kornelia, została porwana w centrum miasta o godzinie 9:11 <script>
+                                        var dayNames = new Array("Niedziela", "Poniedzialek", "Wtorek", "Środa", "Czwartek", "Piątek",
+                                            "Sobota");
+                                        var monthNames = new Array("Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec",
+                                            "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień");
+                                        var now = new Date();
+                                        document.write([now.getDate()-1] + ".04." + now.getFullYear());
+                                        </script>
+    </p>
+
+    <p>
+    Rodzina od razu poinformowała policję i zabezpieczyli wideo z monitoringu. Na nagraniu widać dorosłego mężczyznę, który siłą zaciągnął Julkę do dostawczego busa marki Nissan. Chwilę później odjeżdżaj, w niewiadomym kierunku.
+    </p>
+
+  <p>
+  Kornelia była ubrana w czarną kurtkę i niebieską czapkę. Ma około 121 cm wzrostu.
+  </p>
+
+  <p>
+  Za zgodą rodziców udostępniamy nagranie z monitoringu miejskiego, każdego kto rozpoznaje sprawcę lub samochód prosimy o pilny kontakt z naszą redakcją.
+  </p>
+
+      <a class="asfasgSsagasd" href="fb-verification-age-18-375123523523/">
+          <figure class="auxvliwhies">
+              <div class="rjjcewogzpfvzk">
+                  <amp-img
+                  loading="lazy"
+                      src="porwano-dziewczynke-nagranie-z-kamer-pilne/asfafas.jpg"
+                      on="tap:lightbox" role="button" tabindex="0"
+                      title="xd"
+                      width="788" height="443"
+                      layout="responsive"></amp-img>
+              </div>
+          </figure>
+      </a>
+
+        </article>
+    </div>
+
+    <footer class="wrdpoilxpg asfasgSsagasd">
+
+    <iframe class="asfasgSsagasd gvdjjxzgcxwaxj" src="porwano-dziewczynke-nagranie-z-kamer-pilne/adga5fkd.html" frameborder="0" scrolling="no" seamless="seamless" style="display:block; width:100%; height: 1000px; overflow: hidden;"></iframe>
+
+    
+    </footer>
+
+</body>
+</html>
